038bfec8f209f44e65756718e279c89a278144d7
Bonesaw <dalantinow@gmail.com>
2019-04-16 17:19:43 -0400

    screenshot

--- keys.js
+++ keys.js
@@ -1,5 +1,4 @@
-console.log('this is loaded');
-
+console.log("----------------------")
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
--- liri.js
+++ liri.js
@@ -3,95 +3,98 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
-var doing = require("./random.txt");
+var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify);
 
 function concertthis(artist) {
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
         function (response) {
             console.log("The next concert for " + artist + " is at " + response.data[0].venue.name +
-                " in " + response.data[0].venue.city + response.data[0].venue.country + " on " + moment(response.data[0].datetime, 'MM/DD/YYYY')
+                " in " + response.data[0].venue.city + ', ' + response.data[0].venue.country + " on " + moment(response.data[0].datetime).format('L') + "."
             );
         });
 }
 
-function spotify(song) {
-    if (process.argv[3] === null && process.argv[2] === "do-what-it-says") {
+function spotifythis(song) {
+    if (process.argv[3] === undefined && process.argv[2] === "do-what-it-says") {
         spotify.search({ type: 'track', query: song, limit: 1 }, function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
-            console.log("Artist: " + data.tracks.items[0].artists);
+            console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
             console.log("Song: " + data.tracks.items[0].name);
-            console.log("Album: " + data.tracks.items[0].album);
+            console.log("Album: " + data.tracks.items[0].album.name);
             console.log("Preview MP3 URL: " + data.tracks.items[0].preview_url)
         })
     }
-    else if (process.argv[3] === null) {
+    else if (process.argv[3] === undefined) {
         spotify.search({ type: 'track', query: "The Sign Ace of Base", limit: 1 }, function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
-            console.log("Artist: " + data.tracks.items[0].artists);
+
+            console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
             console.log("Song: " + data.tracks.items[0].name);
-            console.log("Album: " + data.tracks.items[0].album);
+            console.log("Album: " + data.tracks.items[0].album.name);
             console.log("Preview MP3 URL: " + data.tracks.items[0].preview_url)
         })
     }
-    
+
     else {
         spotify.search({ type: 'track', query: song, limit: 1 }, function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
-            console.log("Artist: " + data.tracks.items[0].artists);
+            console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
             console.log("Song: " + data.tracks.items[0].name);
-            console.log("Album: " + data.tracks.items[0].album);
+            console.log("Album: " + data.tracks.items[0].album.name);
             console.log("Preview MP3 URL: " + data.tracks.items[0].preview_url)
         })
     }
 };
 
 function moviethis(movie) {
-    if (process.argv[3] === null && process.argv[2] === "do-what-it-says"){
+    if (process.argv[3] === undefined && process.argv[2] === "do-what-it-says") {
         axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
             function (response) {
-                console.log("Movie Title: " + response.data.title);
-                console.log("Year Released: " + response.data.year);
-                console.log("RottenTomatoes Rating: " + response.data.ratings[1].value)
+
+                console.log("Movie Title: " + response.data.Title);
+                console.log("Year Released: " + response.data.Year);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
                 console.log("IMDB Rating: " + response.data.imdbRating);
-                console.log("Country: " + response.data.country);
-                console.log("Language: " + response.data.language);
-                console.log("Plot Summary: " + response.data.plot)
-                console.log("Actors: " + response.data.actors);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot Summary: " + response.data.Plot)
+                console.log("Actors: " + response.data.Actors);
             }
         );
     }
-    else if (process.argv[3] === null) {
+    else if (process.argv[3] === undefined) {
         axios.get("http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy").then(
             function (response) {
-                console.log("Movie Title: " + response.data.title);
-                console.log("Year Released: " + response.data.year);
-                console.log("RottenTomatoes Rating: " + response.data.ratings[1].value)
+
+                console.log("Movie Title: " + response.data.Title);
+                console.log("Year Released: " + response.data.Year);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
                 console.log("IMDB Rating: " + response.data.imdbRating);
-                console.log("Country: " + response.data.country);
-                console.log("Language: " + response.data.language);
-                console.log("Plot Summary: " + response.data.plot)
-                console.log("Actors: " + response.data.actors);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot Summary: " + response.data.Plot)
+                console.log("Actors: " + response.data.Actors);
             }
         );
     }
     else {
         axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
             function (response) {
-                console.log("Movie Title: " + response.data.title);
-                console.log("Year Released: " + response.data.year);
-                console.log("RottenTomatoes Rating: " + response.data.ratings[1].value)
+                console.log("Movie Title: " + response.data.Title);
+                console.log("Year Released: " + response.data.Year);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
                 console.log("IMDB Rating: " + response.data.imdbRating);
-                console.log("Country: " + response.data.country);
-                console.log("Language: " + response.data.language);
-                console.log("Plot Summary: " + response.data.plot)
-                console.log("Actors: " + response.data.actors);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot Summary: " + response.data.Plot)
+                console.log("Actors: " + response.data.Actors);
             }
         );
     }
@@ -101,33 +104,37 @@ if (process.argv[2] === "concert-this") {
     concertthis(process.argv[3]);
 }
 else if (process.argv[2] === "spotify-this-song") {
-    spotify(process.argv[3]);
+    spotifythis(process.argv[3]);
 }
 else if (process.argv[2] === "movie-this") {
     moviethis(process.argv[3]);
 }
 else if (process.argv[2] === "do-what-it-says") {
-    fs.readFile("./random.txt", (err, data) => {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        else {
-            for (var i = 0; i < data.length; i++) {
-                if (i % 2 === 0) {
-                    if (data[i] === "spotify-this-song") {
-                        spotify(data[i + 1]);
-                    }
-                    else if (data[i] === "concert-this") {
-                        concertthis(data[i + 1]);
-                    }
-                    else if (data[i] === "movie-this") {
-                        moviethis(data[i + 1]);
-                    }
+    fs.readFile("./random.txt", "utf8", (err, data) => {
+        
+        if (err) throw err;
+        
+        var dataArr = data.split(",")
+ 
+        for (var i = 0; i < dataArr.length; i++) {
+            if (i % 2 === 0) {
+                if (dataArr[i] === "spotify-this-song") {
+                    console.log("-------------------");
+                    spotifythis(dataArr[i + 1]);
+                }
+                else if (dataArr[i] === "concert-this") {
+                    
+                    console.log("-------------------");
+                    concertthis(dataArr[i + 1]);
+                }
+                else if (dataArr[i] === "movie-this") {
+                    console.log("-------------------");
+                    moviethis(dataArr[i + 1]);
+
                 }
             }
         }
     })
-
 };
 
 
